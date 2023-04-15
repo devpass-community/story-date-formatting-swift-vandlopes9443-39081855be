6842c0dbcf9b6ccf7b65e08c600eb36834989379
Vanderlei <vandlopes98@gmail.com>
2023-04-15 16:02:20 -0300

    minor fix

--- Sources/DateFormatting/DateFormatting.swift
+++ Sources/DateFormatting/DateFormatting.swift
@@ -3,20 +3,23 @@ import Foundation
 @main
 public struct DateFormatting {
 
-    public let formatter = DateFormatter()
-    formatter.dateFormat = "dd/MM/yyyy"
-
     public static func main() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
         let date = formatter.date(from: "01/01/2022")
 
         print(DateFormatting().formatDate(date: date!))
     }
 
     func formatDate(date: Date) -> String {
-        return formatter.string(from: date)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        return formatter.string(from: dateString)
     }
 
     func dateFromString(dateString: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
         return formatter.date(from: dateString)
     }
 }
